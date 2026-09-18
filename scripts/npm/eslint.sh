4936dc5449ce33f479d49d01a16e4a8fb122f72b
Wikid82 <jhatfield82@gmail.com>
2026-09-18 17:35:56 -0400

    fix(scripts): use npm update instead of install for eslint

--- scripts/npm/eslint.sh
+++ scripts/npm/eslint.sh
@@ -15,5 +15,5 @@ for MODULE in "${NPM_MODULES[@]}"; do
     echo "============================================================================"
 
     cd "$MODULE" || exit 1
-    npm install eslint
+    npm update eslint
 done
