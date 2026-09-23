f319f25a72704a8f9cf220de6c6cebc2168fbece
Jeremy Hatfield <jhatfield82@gmail.com>
2026-09-23 12:00:20 +0000

    chore: bump prettier to 3.9.9

--- scripts/npm/prettier.sh
+++ scripts/npm/prettier.sh
@@ -5,8 +5,6 @@ REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 NPM_MODULES=(
         "$REPO_ROOT"
-        "$REPO_ROOT/frontend"
-        "$REPO_ROOT/docs-site"
     )
 
 for MODULE in "${NPM_MODULES[@]}"; do
